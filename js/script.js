73a6f609c751126922e17d5d2f30bd88ad117c4a
Vahidone <v.ban.roma@gmail.com>
2023-09-06 15:27:32 +0200

    refactor: add variable xNumber to js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-let name, lastname, favoriteColor, message;
+let name, lastname, favoriteColor, message, xNumber;
 
 name = prompt("come ti chiami?");
 lastname = prompt("inserisci il tuo cognome?");
 color = prompt("qual è il tuo colore preferito?");
-
-message = name + lastname + color + 23;
+xNumber = 23;
+message = name + lastname + color + xNumber;
 
 const outputPass = (document.getElementById("passwordClint").innerHTML =
   message);
